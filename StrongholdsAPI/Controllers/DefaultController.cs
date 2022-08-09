fc463f636e169a85a8c2b24ec5920f5990d5100c
Dan Podbury <29648303+danpodbury@users.noreply.github.com>
2022-08-09 16:58:05 +1000

    handle root redirect

--- StrongholdsAPI/Controllers/DefaultController.cs
+++ StrongholdsAPI/Controllers/DefaultController.cs
@@ -11,7 +11,15 @@ namespace StrongholdsAPI.Controllers
         {
             _logger = logger;
         }
+
+        [HttpGet, Route("/")]
+        public ContentResult Index()
+        {
+            var html = System.IO.File.ReadAllText(@"./Static/Index.html");
         
+            return base.Content(html, "text/html");
+        }
+
         // Returns a list of all stations
         [HttpGet, Route("/Status/")]
         public string GetStatus()
