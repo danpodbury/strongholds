197afabe7d8ddb89946b03ccb9e6e8194b546a15
Dan Podbury <29648303+danpodbury@users.noreply.github.com>
2022-08-21 15:29:26 +1000

    Included API index in build Added error message if API index not found fixes #6

--- StrongholdsAPI/Controllers/DefaultController.cs
+++ StrongholdsAPI/Controllers/DefaultController.cs
@@ -15,9 +15,15 @@ namespace StrongholdsAPI.Controllers
         [HttpGet, Route("/")]
         public ContentResult Index()
         {
-            var html = System.IO.File.ReadAllText(@"./Static/Index.html");
-        
-            return base.Content(html, "text/html");
+            if (System.IO.File.Exists(@"./Static/Index.html"))
+            {
+                var html = System.IO.File.ReadAllText(@"./Static/Index.html");
+                return base.Content(html, "text/html");
+            } else
+            {
+                return base.Content("Could not find API docs.", "text/html");
+            }
+
         }
 
         // Returns a list of all stations
